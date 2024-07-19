b2d1e9b613348999c3d1f1e4138703fb1e5a8cc2
manzanareszarate <124327247+manzanareszarate@users.noreply.github.com>
2024-07-19 11:01:52 -0600

    primer comit

--- sistema/salud/views.py
+++ sistema/salud/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1> Santiago Santamaria <h1>")
+    return render (request, 'paginas/inicio.html')
