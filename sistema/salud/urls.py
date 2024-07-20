f3aee0dece0293d80a1333f85b15894b0fb294c7
manzanareszarate <124327247+manzanareszarate@users.noreply.github.com>
2024-07-19 18:09:25 -0600

    comit 2

--- sistema/salud/urls.py
+++ sistema/salud/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
 path('', views.inicio, name='inicio'),
+path('soporte/', views.soporte, name='soporte'),
+path('index/', views.index, name='index'),
+path('agregar/', views.agregar, name='agregar'),
+path('editar/', views.editar, name='editar')
 ]
+
+
--- sistema/salud/views.py
+++ sistema/salud/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def inicio(request):
     return render (request, 'paginas/inicio.html')
+
+def soporte(request):
+    return render (request, 'paginas/soporte.html')
+
+def index(request):
+    return render (request, 'Programar/index.html')
+
+def agregar(request):
+    return render (request, 'Programar/agregar.html')
+
+def editar(request):
+    return render (request, 'Programar/editar.html')
